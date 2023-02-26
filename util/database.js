e8129ce9d971ad6f04adb40030042ad0222e4828
NAJOOYEONG <sklwndud@ajou.ac.kr>
2023-02-26 12:33:13 +0900

    FEAT:Sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require('sequelize');
 
-//connection pool
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "1234",
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
